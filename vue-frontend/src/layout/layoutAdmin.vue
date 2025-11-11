7fd53c24b3a3c82aacc7ecea3e41304b41033d69
Your Name <hieuhtpk04060@gmail.com>
2025-11-11 17:39:46 +0700

    fix Name

--- vue-frontend/src/layout/layoutAdmin.vue
+++ vue-frontend/src/layout/layoutAdmin.vue
@@ -1,15 +1,13 @@
 <script setup>
 import { onMounted, onUnmounted } from 'vue';
-import AdminHeader from '../components/admin/adminHeader.vue';
-import AdminFooter from '../components/admin/adminFooter.vue';
-import AdminSidebar from '../components/admin/AdminSidebar.vue'; // Import Sidebar mới tạo
+import AdminHeader from '../components/admin/AdminHeader.vue';
+import AdminFooter from '../components/admin/AdminFooter.vue';
+import AdminSidebar from '../components/admin/AdminSidebar.vue'; 
 
-// Thêm các class cần thiết cho body khi vào trang admin
 onMounted(() => {
   document.body.classList.add('layout-fixed', 'sidebar-expand-lg', 'bg-body-tertiary');
 });
 
-// Xóa các class khi rời khỏi trang admin để không ảnh hưởng trang user
 onUnmounted(() => {
   document.body.classList.remove('layout-fixed', 'sidebar-expand-lg', 'bg-body-tertiary');
 });
